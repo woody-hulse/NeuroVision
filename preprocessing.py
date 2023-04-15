fcfdec6b93a5fe2305b7166d5649f53a9c93b7eb
woody-hulse <wood_hulse@brown.edu>
2023-04-15 13:54:28 -0400

    behavioral preprocessing pt 1

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@ import os
 import mne
 import gzip
 import shutil
+import csv
 from tqdm import tqdm
 
 """
@@ -99,29 +100,251 @@ def compress_preprocessed_EEG(filepath, patientID):
         os.mkdir(filepath + "../" + EEG_RESULT_DIR)
     np.save(filepath + "../" + EEG_RESULT_DIR + patientID + ".npy", eeg[border:border + trim])
 
-def preprocess(filepath):
+
+def get_behavioral_test(filepath, test):
+    """
+    extracts the behavioral information from patients
+
+    filepath    : path to data folder
+    sync        : preprocess only paired patient info
+    return      : dictionary of behavioral scores for given metric
+    """
+
+    BEHAVIORAL_DIR = filepath + "behavioral/Cognitive_Test_Battery_LEMON/"
+
+    behavioral_dict = {}
+    filename = ""
+    cols = []
+
+    test = test.lower()
+    if test == "cvlt":
+        """
+        1= test version of the CVLT
+        2= number of correct  recalls after hearing the list for the first time
+        3= number of correct  recalls after hearing the list for the fifth time
+        4= Proactive Interference (Interference from previosly learned information)
+        5= Retroactive Interference
+        6= sum of all correct recalls from first until fifth trial
+        7= sum of all correct recalls from first until fith trial with an age-gender-education correction
+        8= number of correct recalls from the interference task (List B)
+        9= number of correct recalls (short delay)
+        10= number of correct recalls when category cues are presented (short delay)
+        11= number of correct recalls after 20 minutes (long delay recall) 
+        12= number of correct recalls when category cues are presented (long delay recall)
+        13= delayed recognition memory performance
+        14= overall repetitions
+        15=overall intrusions
+        16= comments
+        """
+
+        filename = "CVLT/CVLT.csv"
+        cols = [4, 10, 11, 13]
+        # notes:
+
+    elif test == "lps":
+        """
+        LPS_1 = LPS raw data, how many symbol-rows did the participant process correctly
+        LPS_2 = comments
+        """
+        filename = "LPS/LPS.csv"
+        cols = [1]
+        # notes: 
+
+    elif test == "rwt":
+        """
+        S-words
+        RWT_1 = how many s-words did the participant name during the first minute (according to rules)
+        RWT_2 = percentile rank of correct words for the subtest 's-words' (1 minute)
+        RWT_3 = how many repetitions during the first minute
+        RWT_4 = how many rule breaks during the first minute
+        RWT_5 = how many s-words did the participant name during the second minute (according to rules)
+        RWT_6 = how many repetitions during the second minute
+        RWT_7 = how many rule breaks during the second minute
+        RWT_8 = how many s-words did the pp. name in total (according to rules); two minutes
+        RWT_9 = percentile rank of correct words for the subtest 's-words' (2 minutes)
+        RWT_10 = how many repetitions in total; two minutes
+        RWT_11 = how many rule breaks in total; two minutes
+        RWT_12 = comments (s-words)
+
+        Animals
+        RWT_13 = how many animals did the participant. name in the first minute (according to rules)
+        RWT_14 = percentile rank of correct words for the subtest 'animal' (1 minute)
+        RWT_15 = how many repetitions during the first minute (animals)
+        RWT_16 = how many rule breaks during the first minute (animals)
+        RWT_17 = how many animals did the pp. name during the second minute (according to rules)
+        RWT_18 = how many repetitions during the second minute (animals)
+        RWT_19 = how many rule breaks during the second minute (animals)
+        RWT_20 = how many animals did the pp. name in total (according to rules); two minutes
+        RWT_21 = percentile rank of correct words for the subtest 'animals' (2 minutes)
+        RWT_22 = how many repetitions in total; two minutes (animals)
+        RWT_23 = how many rule breaks during the second minute (animals)
+        RWT_24 = comments (animals)
+        """
+
+        filename = "RWT/RWT.csv"
+        cols = [8, 20]
+        # notes: add cols 8 and 20?
+
+    elif test == "tap":
+        dict1 = get_behavioral_test(filepath, "tap-alertness")
+        dict2 = get_behavioral_test(filepath, "tap-incompatibility")
+        dict3 = get_behavioral_test(filepath, "tap-working")
+        
+        # notes: combine dicts instead of returning separately?
+        return [dict1, dict2, dict3]
+    
+    elif test == "tap-alertness":
+        """
+        TAP_A_1 = reaction time medians for 1. round (no signal)
+        TAP_A_2 = reaction time medians for 2. round (signal)
+        TAP_A_3 = reaction time medians for 3. round (signal)
+        TAP_A_4 = reaction time medians for 4. round (no signal)
+
+        NO signal (aggregate scores)
+        TAP_A_5 = mean reaction time (no signal)
+        TAP_A_6 = median reaction time (no signal)
+        TAP_A_7 = % (no signal)
+        TAP_A_8 = standard deviations (no signal)
+        TAP_A_9 = % (no signal)
+
+        Signal (aggregate scores)
+        TAP_A_10 = mean reaction time (signal)
+        TAP_A_11 = median reaction time (signal)
+        TAP_A_12 = % (signal)
+        TAP_A_13 = standard deviations (signal)
+        TAP_A_14 = % (signal)
+
+        Phasic alertness
+        TAP_A_15 = phasic alertness
+        TAP_A_16 = % (phasic alertness)
+        TAP_A_17 = comments
+        """
+
+        filename = "TAP_Alertness/TAP-Alertness.csv"
+        cols = [5]
+
+        # notes: i think for dependency reasons we only need to choose 1 (with/without signal)
+
+    elif test == "tap-incompatibility":
+        """
+        Compatible stimuli
+        TAP_I_1 = mean time for compatible stimuli
+        TAP_I_2 = median time for compatible stimuli
+        TAP_I_3 = %
+        TAP_I_4 = standard deviations
+        TAP_I_5 = %
+        TAP_I_6 = how many errors did participant make during compatible stimuli presentation
+        TAP_I_7 = %
+
+        Incompatible stimuli
+        TAP_I_8 = mean time for incompatible stimuli
+        TAP_I_9 = median time for incompatible stimuli
+        TAP_I_10 = %
+        TAP_I_11 = standard deviations
+        TAP_I_12 = %
+        TAP_I_13 = how many errors did participant make during incompatible stimuli presentation
+        TAP_I_14 = %
+
+        Whole stimuli
+        TAP_I_15 = mean time for whole stimuli presentation
+        TAP_I_16 = median time for whole stimuli presentation
+        TAP_I_17 = %
+        TAP_I_18 = standard deviations
+        TAP_I_19 = %
+        TAP_I_20 = how many errors did participant make during whole stimuli presentation
+        TAP_I_21 = %
+
+        F-values
+        TAP_I_22 = F-value_visual_field
+        TAP_I_23 = %
+        TAP_I_24 = F-value_hand
+        TAP_I_25 = %
+        TAP_I_26 = F-value_visual_field_x_hand
+        TAP_I_27 = %
+        TAP_I_28 = comments
+        """
+
+        filename = "TAP_Incompatibility/TAP-Incompatibility.csv"
+        cols = [15]
+
+        # notes:
+    
+    elif test == "tap-working":
+        """
+        TAP_WM_1 = mean reaction time for correct pressed bottoms (correct answers)
+        TAP_WM_2 = reaction time median
+        TAP_WM_3 = %
+        TAP_WM_4 = standard deviations
+        TAP_WM_5 = %
+        TAP_WM_6 = how many correct matches
+        TAP_WM_7 = how many incorrect matches (errors)
+        TAP_WM_8 = %
+        TAP_WM_9 = how many missed matches (omissions)
+        TAP_WM_10 = %
+        TAP_WM_11 = outliers
+        TAP_WM_12 = comments
+        """
+
+        filename = "TAP_Working_Memory/TAP-Working Memory.csv"
+        cols = [1]
+
+        # notes: should really be wm2 / (wm6 / (wm6 + wm7 + wm9)) 
+
+    else:
+        print("behavioral test not configured :", test)
+        return
+
+    with open(BEHAVIORAL_DIR + filename, 'r') as fin:
+        data = csv.reader(fin)
+        next(data, None)  # skip header
+        behavioral_dict = {rows[0]:[rows[col] for col in cols] for rows in data}
+    return behavioral_dict
+
+
+def preprocess_behavioral_dict(behavioral_dict):
+    """
+    convert dict values (string) to standardized (percentile) floats
+    filter unfilled columns (set to 0.5?)
+    """
+
+    return None
+
+
+def preprocess(filepath, sync=False):
     """
     preprocess and link all MRI and EEG data 
 
     filepath    : path to data folder
+    sync        : preprocess only paired patient info
     return      : none
     """
 
     print("preprocessing data from", filepath, "...")
     
     mri_patientIDs = set(os.listdir(filepath + MRI_DIR))
+    mri_patientIDs.remove(".DS_Store")
     eeg_patientIDs = set(os.listdir(filepath + EEG_DIR))
-    patientIDs = mri_patientIDs.intersection(eeg_patientIDs)
-    patientIDs.remove(".DS_Store")
-    for patientID in patientIDs:
-        compress_MRI(filepath + MRI_DIR, patientID)
-        compress_preprocessed_EEG(filepath + EEG_DIR, patientID)
+    eeg_patientIDs.remove(".DS_Store")
+    if sync:
+        patientIDs = mri_patientIDs.intersection(eeg_patientIDs)
+        for patientID in tqdm(patientIDs):
+            compress_MRI(filepath + MRI_DIR, patientID)
+            compress_preprocessed_EEG(filepath + EEG_DIR, patientID)
+    else:
+        print("preprocessing MRI data ...")
+        for mri_patientID in tqdm(mri_patientIDs):
+            compress_MRI(filepath + MRI_DIR, mri_patientID)
+        print("preprocessing EEG data ...")
+        for eeg_patientID in tqdm(eeg_patientIDs):
+            compress_preprocessed_EEG(filepath + EEG_DIR, eeg_patientID)
+
 
 def main():
     """
     for testing
     """
-    preprocess(DATA_PATH)
+    # preprocess(DATA_PATH)
+    get_behavioral_test(DATA_PATH, "tap")
 
 if __name__ == "__main__":
     main()
